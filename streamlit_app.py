3d93e67ddcdae57ac7c4160da6cf85686f12adb1
Jamil Abo-Alrub <jamil.aboalrub@gmail.com>
2025-02-03 16:59:34 +0100

    np

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,32 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
-import cv2
 import tempfile
-import matplotlib
+import scipy.cluster.vq as vq
 
 def segment_image(image_path, k):
     # Read in the image
-    image = cv2.imread(image_path)
-    
-    if image is None:
-        raise ValueError("Image not found or unable to read")
-    
-    # Convert BGR to RGB
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = Image.open(image_path)
+    image = image.convert("RGB")  # Ensure it's in RGB mode
+    image_np = np.array(image)
     
     # Reshape image into 2D array of pixels and 3 color values (RGB)
-    pixel_vals = image.reshape((-1, 3))
+    pixel_vals = image_np.reshape((-1, 3))
     
     # Convert to float type
     pixel_vals = np.float32(pixel_vals)
     
-    # Define criteria for stopping the algorithm
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.85)
-    
     # Perform k-means clustering
-    retval, labels, centers = cv2.kmeans(pixel_vals, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    centers, labels = vq.kmeans2(pixel_vals, k, iter=10, minit='random')
     
     # Convert data into 8-bit values
     centers = np.uint8(centers)
-    segmented_data = centers[labels.flatten()]
+    segmented_data = centers[labels]
     
     # Reshape data into the original image dimensions
-    segmented_image = segmented_data.reshape(image.shape)
+    segmented_image = segmented_data.reshape(image_np.shape)
     
-    return image, segmented_image
+    return image_np, segmented_image
 
 def main():
     st.title("Image Segmentation App")
